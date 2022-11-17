2a184599abd2f7c2fda4c88404a035054d333ac2
LucasGX <gonzalezlucasx@gmail.com>
2022-11-17 14:35:40 -0300

    startpoint

--- script/insertData.js
+++ script/insertData.js
@@ -1,20 +1,25 @@
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { ddbDocClient } from "./client.js";
+// import { PutCommand } from "@aws-sdk/lib-dynamodb";
+// import { ddbDocClient } from "./client.js";
+const { PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { ddbDocClient } = require("./client.js");
 
 export const putItem = async () => {
-    const params = {
-        TableName: "AutoSolve_DB",
-        Item: {
-        primaryKey: 1,
-        sortKey: "Comida",
-        },
-    };
-    try {
-        console.log("Adding a new item...", params);
-        const data = await ddbDocClient.send(new PutCommand(params));
-        console.log("Success - item added or updated", data);
-    } catch (err) {
-        console.log("Error", err.stack);
-    }
+	const params = {
+		TableName: "AutoSolve_DB",
+		Item: {
+			primaryKey: 1,
+			sortKey: "Comida",
+		},
+	};
+	try {
+		console.log("Adding a new item...", params);
+		const data = await ddbDocClient.send(new PutCommand(params));
+		console.log("Success - item added or updated", data);
+	} catch (err) {
+		console.log("Error", err.stack);
+	}
 };
-putItem();
+window.onload = () => {
+	console.log("Hello World");
+	document.getElementById("boton").addEventListener("click", putItem);
+};
